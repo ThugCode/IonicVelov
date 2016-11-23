0b6e7875d53e81883fa0cdf0588a4fbb56bec36d
Letourneur Léo <letourneur.leo@gmail.com>
2016-11-23 14:17:12 +0100

    Map

--- src/pages/localisation/localisation.ts
+++ src/pages/localisation/localisation.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
+import { Geolocation } from 'ionic-native';
+
+declare var ol: any;
 
 @Component({
   selector: 'localisation-page',
   templateUrl: 'localisation.html'
 })
 export class LocalisationPage {
-  constructor() {
 
+  @ViewChild('map') map;
+
+  constructor() {
+    
   }
+
+  ngAfterViewInit() {
+        
+        var lat = 0;
+        var long = 0;
+        Geolocation.getCurrentPosition().then((resp) => {
+          lat = resp.coords.latitude;
+          long = resp.coords.longitude;
+        }).catch((error) => {
+          console.log('Error getting location', error);
+        });
+
+        new ol.Map({
+            target: "map",
+            layers: [
+              new ol.layer.Tile({
+                source: new ol.source.OSM() 
+              })
+            ],
+            view: new ol.View({
+              center: [lat, long],
+              zoom: 10
+            })
+        });
+
+        
+
+    }
+
+    
 }
