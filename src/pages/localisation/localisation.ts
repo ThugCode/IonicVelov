97d7654c19bb96c4818a3603ccff1730a86068c0
Letourneur Léo <letourneur.leo@gmail.com>
2016-11-25 17:29:23 +0100

    Fin map / Début marker

--- src/pages/localisation/localisation.ts
+++ src/pages/localisation/localisation.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-
 import { Geolocation } from 'ionic-native';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 declare var ol: any;
 
@@ -11,38 +12,53 @@ declare var ol: any;
 export class LocalisationPage {
 
   @ViewChild('map') map;
+  movies: Array<any>;
 
-  constructor() {
-    
+  constructor(public http: Http) {
+    this.loadMap();
+
+    var url = "https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json";
+    this.http.get(url)
+        .map(res => res.json())
+        .subscribe(data => {
+            this.movies = data.values;
+            console.log(data.fields)
+        });
   }
 
-  ngAfterViewInit() {
-        
-        var lat = 0;
-        var long = 0;
+  loadMap() {
         Geolocation.getCurrentPosition().then((resp) => {
-          lat = resp.coords.latitude;
-          long = resp.coords.longitude;
+          var long = resp.coords.longitude;
+          var lat = resp.coords.latitude;
+          
+          var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point(ol.proj.fromLonLat([long, lat])),
+            name: 'Ma position'
+          });
+
+          var vectorSource = new ol.source.Vector({
+            features: [iconFeature]
+          });
+          
+          var vectorLayer = new ol.layer.Vector({
+            source: vectorSource
+          });
+
+          var mapImg = new ol.layer.Tile({
+            source: new ol.source.OSM() 
+          });
+          
+          new ol.Map({
+            target: "map",
+            layers: [ mapImg, vectorLayer ],
+            view: new ol.View({
+              center: ol.proj.fromLonLat([long, lat]),
+              zoom: 15
+            })
+        })
+
         }).catch((error) => {
           console.log('Error getting location', error);
-        });
-
-        new ol.Map({
-            target: "map",
-            layers: [
-              new ol.layer.Tile({
-                source: new ol.source.OSM() 
-              })
-            ],
-            view: new ol.View({
-              center: [lat, long],
-              zoom: 10
-            })
-        });
-
-        
-
+        })
     }
-
-    
 }
