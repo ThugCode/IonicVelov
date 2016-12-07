bdc74486a92d70e142bd972c1c74a248d57774c5
Letourneur Léo <letourneur.leo@gmail.com>
2016-12-07 21:36:38 +0100

    Popup du turf

--- src/pages/localisation/localisation.ts
+++ src/pages/localisation/localisation.ts
@@ -4,30 +4,29 @@ import { Geolocation } from 'ionic-native';
 import { StationService } from '../../app/station.service';
 import { Station } from '../../app/station';
 import { LoadingController } from 'ionic-angular';
+import { File } from 'ionic-native';
+import { Platform } from 'ionic-angular';
 
 import ol from 'openlayers';
+declare var cordova: any;
 
 @Component({
   selector: 'localisation-page',
   templateUrl: 'localisation.html'
 })
 export class LocalisationPage implements OnInit {
-  @ViewChild('map') map;
-  stations: Station[];
-  loader:any;
+  @ViewChild('map') mapChild;           //Child map in HTML
+  loader: any;                           //Loader
+  stations: Station[];                  //Station list
+  stationSelected: any;                 //Selected station
+  mapDiv: any;                           //Div of map
+  popupDiv: any;                         //Div of popup
 
   constructor(
     private stationService: StationService,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private platform: Platform
   ) { }
-
-  getStations() {
-    this.stationService.getStations()
-      .subscribe(stations => {
-        this.stations = stations;
-        this.loadMap();
-      });
-  }
 
   ngOnInit() {
     this.loader = this.loadingCtrl.create({
@@ -38,148 +37,151 @@ export class LocalisationPage implements OnInit {
     this.getStations();
   }
 
-  getStyle(color) {
-    return new ol.style.Style({
-        image: new ol.style.Circle({
-          radius: 6,
-          stroke: new ol.style.Stroke({
-            color: 'white',
-            width: 2
-          }),
-          fill: new ol.style.Fill({ color: color })
-        })
+  getStations() {
+    this.stationService.getStations()
+      .subscribe(stations => {
+        this.stations = stations;
+        this.loadMap();
       });
   }
 
   loadMap() {
-
-    var fS_Closed: Array<any>;
-    var fS_Empty: Array<any>;
-    var fS_Full: Array<any>;
-    var fS_Available: Array<any>;
-    var fS_MyPosition: Array<any>;
-    fS_Closed = [];
-    fS_Empty = [];
-    fS_Full = [];
-    fS_Available = [];
-    fS_MyPosition = [];
-
     Geolocation.getCurrentPosition().then((resp) => {
-      var long = resp.coords.longitude;
-      var lat = resp.coords.latitude;
-      var tempFeature;
       var prefered = ["768", "844", "923"];
-      
-      this.stations.forEach(element => {
-
-        tempFeature = new ol.Feature({
-          geometry: new ol.geom.Point(ol.proj.fromLonLat([parseFloat(element.lng), parseFloat(element.lat)])),
-          name: element.name,
-          status: element.status,
-          bikeStands: element.bike_stands,
-          available: element.available_bike_stands,
-          favorite: prefered.indexOf(element.gid) >= 0
-        });
-
-        if(element.status == "CLOSED") {
-          fS_Closed.push(tempFeature);
-        } else if(element.available_bikes == "0") {
-          fS_Empty.push(tempFeature);
-        } else if(element.available_bike_stands == "0") {
-          fS_Full.push(tempFeature);
-        } else {
-          fS_Available.push(tempFeature);
-        }
-      });
-      
-      fS_MyPosition.push(new ol.Feature({
-        geometry: new ol.geom.Point(ol.proj.fromLonLat([long, lat])),
-        name: 'Ma position'
-      }));
-
-      var vS_Closed = new ol.source.Vector({ features: fS_Closed });
-      var vS_Empty = new ol.source.Vector({ features: fS_Empty });
-      var vS_Full = new ol.source.Vector({ features: fS_Full });
-      var vS_Available = new ol.source.Vector({ features: fS_Available });
-      var vS_MyPosition = new ol.source.Vector({ features: fS_MyPosition });
-
-      var vL_Closed = new ol.layer.Vector({ source: vS_Closed, style : this.getStyle("red") });
-      var vL_Empty = new ol.layer.Vector({ source: vS_Empty, style : this.getStyle("orange") });
-      var vL_Full = new ol.layer.Vector({ source: vS_Full, style : this.getStyle("yellow") });
-      var vL_Available = new ol.layer.Vector({ source: vS_Available, style : this.getStyle("green") });
-      var vL_MyPosition = new ol.layer.Vector({ source: vS_MyPosition, style : this.getStyle("blue") });
-
-      var mapImg = new ol.layer.Tile({
-        source: new ol.source.OSM()
-      });
-      
-      //***************** POPUP *******************//
-      var container = document.getElementById('popup');
-      var content = document.getElementById('popup-content');
-      var closer = document.getElementById('popup-closer');
-      var starFull = document.getElementById('star-full');
-      var starEmpty = document.getElementById('star-empty');
-
-      var popup = new ol.Overlay({
-        element: container,
-        positioning: 'center-center',
-        stopEvent: false
-      });
-
-      closer.onclick = function() {
-        popup.setPosition(undefined);
-        closer.blur();
-        return false;
-      };
-
-      var map = new ol.Map({
-        target: "map",
-        layers: [mapImg, vL_Closed, vL_Empty, vL_Full, vL_Available, vL_MyPosition],
-        overlays: [popup],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([long, lat]),
-          zoom: 13
-        })
-      })
-
-      // display popup on click
-      map.on('click', function(evt) {
-        var feature = map.forEachFeatureAtPixel(evt.pixel,
-            function(feature, layer) {
-              return feature;
-            });
-        if (feature) {
-          var coord = evt.coordinate;
-          var name = feature.get("name");
-          var status = feature.get("status") == "OPEN" ? "Ouverte" : "Fermée";
-          var bikestand = feature.get("bikeStands");
-          var available = feature.get("available");
-          var favorite = feature.get("favorite");
-
-          content.innerHTML = '<div class="popup-content-name">' + name + '</div>';
-          if(name != "Ma position") {
-            content.innerHTML += '<div class="popup-content-info">'+ 
-                                    '<div class="content-status">' + status + ' </div> ' + 
-                                    '<div class="content-number">' +available + '/' + bikestand + '</div>' +
-                                  '</div>';
-          }
-          
-          if(favorite) {
-            starFull.style.display = "inline-block";
-            starEmpty.style.display = "none";
-          } else {
-            starFull.style.display = "none";
-            starEmpty.style.display = "inline-block";
-          }
-
-          popup.setPosition(coord);
-        }
-      });
-
+      this.createMap(resp, prefered);
       this.loader.dismiss();
-
     }).catch((error) => {
       console.log('Error getting location', error);
     })
   }
+
+  createMap(resp, prefered) {
+
+    var long = resp.coords.longitude;
+    var lat = resp.coords.latitude;
+
+    var tempFeature;
+    var fS_Closed = [];
+    var fS_Empty = [];
+    var fS_Full = [];
+    var fS_Available = [];
+    var fS_MyPosition = [];
+
+    this.stations.forEach(element => {
+
+      tempFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([parseFloat(element.lng), parseFloat(element.lat)])),
+        name: element.name,
+        status: element.status,
+        bikeStands: element.bike_stands,
+        available: element.available_bike_stands,
+        gid: element.gid,
+        lat: element.lat,
+        lng: element.lng,
+        favorite: prefered.indexOf(element.gid) >= 0
+      });
+
+      if (element.status == "CLOSED") {
+        fS_Closed.push(tempFeature);
+      } else if (element.available_bikes == "0") {
+        fS_Empty.push(tempFeature);
+      } else if (element.available_bike_stands == "0") {
+        fS_Full.push(tempFeature);
+      } else {
+        fS_Available.push(tempFeature);
+      }
+    });
+
+    fS_MyPosition.push(new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([long, lat])),
+      name: 'Ma position'
+    }));
+
+    var vS_Closed = new ol.source.Vector({ features: fS_Closed });
+    var vS_Empty = new ol.source.Vector({ features: fS_Empty });
+    var vS_Full = new ol.source.Vector({ features: fS_Full });
+    var vS_Available = new ol.source.Vector({ features: fS_Available });
+    var vS_MyPosition = new ol.source.Vector({ features: fS_MyPosition });
+
+    var vL_Closed = new ol.layer.Vector({ source: vS_Closed, style: this.createStyle("red") });
+    var vL_Empty = new ol.layer.Vector({ source: vS_Empty, style: this.createStyle("orange") });
+    var vL_Full = new ol.layer.Vector({ source: vS_Full, style: this.createStyle("yellow") });
+    var vL_Available = new ol.layer.Vector({ source: vS_Available, style: this.createStyle("green") });
+    var vL_MyPosition = new ol.layer.Vector({ source: vS_MyPosition, style: this.createStyle("blue") });
+
+    var mapImg = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+
+    this.popupDiv = new ol.Overlay({
+      element: document.getElementById('popup')
+    });
+
+    this.mapDiv = new ol.Map({
+      target: "map",
+      layers: [mapImg, vL_Closed, vL_Empty, vL_Full, vL_Available, vL_MyPosition],
+      overlays: [this.popupDiv],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([long, lat]),
+        zoom: 15
+      })//,
+      //controls : [] //Enleve les boutons de controle
+    })
+
+  }
+
+  createStyle(color) {
+    return new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 6,
+        stroke: new ol.style.Stroke({
+          color: 'white',
+          width: 2
+        }),
+        fill: new ol.style.Fill({ color: color })
+      })
+    });
+  }
+
+  clickOnStar(station) {
+    console.log("-------------      CATCH      ------------------------");
+    console.log(station.gid);
+    if (this.platform.is('mobile')) {
+      //this.checkDir(station.gid);
+      this.stationSelected.favorite = !this.stationSelected.favorite;
+    }
+  }
+
+  clickCloser() {
+    this.stationSelected = null;
+  }
+
+  clickMap(event) {
+    var coord = [event.layerX, event.layerY]
+    var feature = this.mapDiv.forEachFeatureAtPixel(coord,
+      function (feature, layer) {
+        return feature;
+      }
+    );
+
+    this.showPopup(feature);
+  }
+
+  showPopup(feature) {
+    if (feature) {
+      this.stationSelected = {};
+      this.stationSelected.name = feature.get("name");
+      this.stationSelected.status = feature.get("status") == "OPEN" ? "Ouverte" : "Fermée";
+      this.stationSelected.bikestand = feature.get("bikeStands");
+      this.stationSelected.available = feature.get("available");
+      this.stationSelected.favorite = feature.get("favorite");
+      this.stationSelected.gid = feature.get("gid");
+
+      var lng = parseFloat(feature.get("lng"));
+      var lat = parseFloat(feature.get("lat"));
+      console.log(ol.proj.fromLonLat([lng, lat]));
+      this.popupDiv.setPosition(ol.proj.fromLonLat([lng, lat]));
+    }
+  }
+
 }
