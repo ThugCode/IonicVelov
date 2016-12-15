5400c33f762e2bf44aede90379b009aee61f5275
Letourneur Léo <letourneur.leo@gmail.com>
2016-12-15 16:48:40 +0100

    Stations favorites à partir du fichier/ Suppression depuis favoris

--- src/app/pages/favorites-list/favorites-list.ts
+++ src/app/pages/favorites-list/favorites-list.ts
@@ -3,12 +3,10 @@ import { NavController, NavParams } from 'ionic-angular';
 import { FavoritesDetailPage } from '../favorites-detail/favorites-detail';
 
 import { StationService } from '../../services/station.service';
+import { FileService } from '../../services/file.service';
 import { Station } from '../../models/station';
-import { File } from 'ionic-native';
 import { Platform } from 'ionic-angular';
 import { ItemSliding } from 'ionic-angular';
-
-declare var cordova: any; 
 
 @Component({
   selector: 'favorites-list-page',
@@ -17,7 +15,6 @@ declare var cordova: any;
 
 export class FavoritesListPage implements OnInit {
 
-  filePath: string;
   selectedItem: any;
   items: Station[];
 
@@ -25,48 +22,50 @@ export class FavoritesListPage implements OnInit {
     private navCtrl: NavController,
     private navParams: NavParams,
     private stationService: StationService,
+    private fileService: FileService,
     private platform: Platform
   ) {
     this.selectedItem = navParams.get('item');
   }
 
-  getFavoris() {
-    this.stationService.getStations()
-      .subscribe(stations => {
-        this.items = [];
+  getStations() {
+    this.stationService.getStations().subscribe(stations => {
+
+      if (this.platform.is('mobile')) {
+        console.log("Mobile device !");
+        this.fileService.readStationFromFile().then(prefered => {
+          this.getFavoris(stations, prefered);
+        });
+      }
+      else {
         var prefered = ["768", "844", "923"];
-        stations.forEach(element => {
-          if (prefered.indexOf(element.gid) >= 0) {
-            element.status = element.status == 'OPEN' ? 'Ouverte' : 'Fermée';
-            this.items.push(element);
-          }
-        })
-      });
+        this.getFavoris(stations, prefered);
+      }
+    });
+  }
+
+  getFavoris(stations, prefered) {
+
+    this.items = [];
+    stations.forEach(element => {
+      if (prefered.indexOf(element.gid) >= 0) {
+        element.status = element.status == 'OPEN' ? 'Ouverte' : 'Fermée';
+        this.items.push(element);
+      }
+    });
   }
 
   ngOnInit() {
-
-    if (this.platform.is('mobile')) {
-
-      this.filePath = cordova.file.dataDirectory;
-      console.log("Mobile device !");
-    } else if(this.platform.is('core')) {
-
-      this.filePath = "../assets/files/";
-      console.log("Computer device !");
-    }
-    
-    //File.checkDir(this.filePath, 'appliVelov').then(_ => console.log('yay1')).catch(err => console.log('boooh1'));
-    //File.createDir(this.filePath, 'appliVelov', true);
-    //File.checkDir(this.filePath, 'appliVelov').then(_ => console.log('yay2')).catch(err => console.log('boooh2'));
-
-    this.getFavoris();
+    this.getStations();
   }
 
-  share(slidingItem: ItemSliding) {
+  delete(slidingItem: ItemSliding, item) {
     slidingItem.close();
+    if (this.platform.is('mobile')) {
+      this.fileService.removeStationToFile(item.gid);
+    }
   }
-  
+
   itemTapped(event, item) {
     this.navCtrl.push(FavoritesDetailPage, {
       item: item
