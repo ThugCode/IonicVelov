7addd7585704a525d385b56647a605f2da3efeb8
LoicGerland <gerlandloic@gmail.com>
2017-01-24 19:00:47 +0100

    Bouton d'affichage des pistes et fin de la branche

--- src/app/services/piste.service.ts
+++ src/app/services/piste.service.ts
@@ -1,18 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
 
 
 @Injectable()
 export class PisteService {
     public PistesUrl = "https://download.data.grandlyon.com/wfs/grandlyon?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&request=GetFeature&typename=pvo_patrimoine_voirie.pvoamenagementcyclable&SRSNAME=urn:ogc:def:crs:EPSG::4326";
 
-    constructor(
-        private http: Http
-    ) { }
-
-    getPistes(): Observable<any> {
-        return this.http.get(this.PistesUrl).map(res => res.json());
-    }
+    constructor() { }
 }
